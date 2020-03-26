39469cd0fd0c4289e1420f736550847b190783fa
matdudq <klatamilata@gmail.com>
2020-03-26 17:36:42 +0100

    Cleaning code

--- Zadanie1/zadanie1.s
+++ Zadanie1/zadanie1.s
@@ -5,86 +5,76 @@ STDIN =0
 STDOUT = 1
 EXIT_SUCCESS = 0
 
-SHIFT_COUNT = 4
-FIRST_ASCII_NUMBER = 0x30
-LAST_ASCII_NUMBER = 0x39
 NUMBERS_ALPHABETH_OFFSET_ASCII = 7
 
 .align 32
 
 .macro exit code
-movl $SYSEXIT, %eax
-movl \code, %ebx
-int $0x80
+	movl $SYSEXIT, %eax
+	movl \code, %ebx
+	int $0x80
 .endm
 
 .macro write str,len
-movl $SYSWRITE, %eax
-movl $STDOUT, %ebx
-movl \str, %ecx
-movl \len, %edx
-int $0x80
+	movl $SYSWRITE, %eax
+	movl $STDOUT, %ebx
+	movl \str, %ecx
+	movl \len, %edx
+	int $0x80
 .endm
 
 .data
-.lcomm msg, 1
-msg_out: .ascii "   "
-msg_out_len = .-msg_out
+	.lcomm msg, 1
+	msg_out: .ascii "   "
+	msg_out_len = .-msg_out
 
 .text
+
 .global _start
 _start:
 
-#instrukcje realizujace wczytywanie ze standardowego wejscia
+#One byte reading from standard input
 read:
-movl $SYSREAD, %eax
-movl $STDIN, %ebx
-movl $msg, %ecx
-movl $1, %edx
-int $0x80
+	movl $SYSREAD, %eax
+	movl $STDIN, %ebx
+	movl $msg, %ecx
+	movl $1, %edx
+	int $0x80
 
-#sprawdzamy czy jest cos jeszcze na wejsciu jesli nie to w rejestrze eax pojawi sie 1 - wartosc bledu dla procedury read
-movl $1, %ebx
-cmpl %eax, %ebx
-jne ex
+#We are comparing read function status, if %eax is not value 1 finish process
+	movl $1, %ebx
+	cmpl %eax, %ebx
+	jne ex
 
-movl $0, %eax #ustawiamy index na 0
-movb msg(,%eax,1) , %bl # wsadzamy 8 bitow liczby do rejestru bl
-movb msg(,%eax,1) , %bh # wsadzamy 8 bitow liczby do rejestru bh
+	movl $0, %eax #setting 0 index for eax register
+	movb msg(,%eax,1) , %bl # setting read byte to register bl
+	movb msg(,%eax,1) , %bh # setting read byte to register bh 
 
-movb $0x30, %dl # ustawiamy dla rejestrow dl wartosci dla poczatku przedzialu
-movb $0x30, %dh # ustawiamy dla rejestrow dh wartosci dla poczatku przedzialu
+	shrb $4, %bl #right shifting bl register to get left letter offset
+	shlb $4, %bh #left shifting bh to leave first 4 bits in register
+	shrb $4, %bh #right shifting bh to get correct rigth letter offset
 
-shrb $SHIFT_COUNT, %bl #przesuwamy w prawo bajt, zostaja nam tylko 4 bity w rejestrze bl
-shlb $SHIFT_COUNT, %bh #przesuwamy w lewo bajt, zostaja nam tylko 4 bity w rejestrze bh
-shrb $SHIFT_COUNT, %bh #przesuwamy w prawo bajt, zeby miec 4 bity na poczatku bh
-#w bl mamy offset lewego znaku
-# w bh mamy offset prawego znaku
+	addb $'0',%bl #adding offset to our base ascii value
+	addb $'0',%bh#same as up
 
-
-addb %dl,%bl # dodajemy poczatek przedzialu wartosci do offsetu
-addb %dh,%bh# to samo
-
-cmpb $LAST_ASCII_NUMBER, %bl #dla bajku bl sprawdzamy czy nie wyszedl poza zakres liczb w Ascii 
-#jesli wyszedl to przesuwamy na litery
-
-jle next
-addb $NUMBERS_ALPHABETH_OFFSET_ASCII, %bl
+	cmpb $'9', %bl #comparing left letter if is outside of desired region
+	jle next #if it is we move it do letters
+	addb $NUMBERS_ALPHABETH_OFFSET_ASCII, %bl
 next:
+	cmpb $'9', %bh
+	jle second_next
+	addb $NUMBERS_ALPHABETH_OFFSET_ASCII, %bh
 
-cmpb $LAST_ASCII_NUMBER, %bh
-jle second_next
-addb $NUMBERS_ALPHABETH_OFFSET_ASCII, %bh
 second_next:
 
+#moving our letters value to output message
+	movb %bl, msg_out(,%eax,1)
+	incl %eax
+	movb %bh, msg_out(,%eax,1)
 
-movb %bl, msg_out(,%eax,1)# przenosimy wartosci znakow do wiadomosci wyjsciowej
-incl %eax
-movb %bh, msg_out(,%eax,1)
+	#sending output message on standard output
+	write $msg_out, $msg_out_len
 
-#wypisujemy na standardowe wyjscie przeliczona wartosc
-write $msg_out, $msg_out_len
-#wracamy do wczytywania
 jmp read
 
 ex:
